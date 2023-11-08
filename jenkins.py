bc42823474b566e2ea4397973ef5e1e62e4b3e63
ChiragMorajkar03 <141605453+ChiragMorajkar03@users.noreply.github.com>
2023-11-08 06:56:49 +0530

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -1,2 +1,10 @@
 print("hello") 
-    
+
+
+
+docker starter ke lia experiment 6-7:   
+https://github.com/aaryaparadkar/docker-starter
+
+
+yeh pipeline i.e exp5
+https://github.com/aaryaparadkar/jenkins_deploy
